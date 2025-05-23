e97eeb11c2a03a5512771c9d713a875acefa600c
anh khoa <pdanhkhoa21@gmail.com>
2025-05-23 15:00:11 +0700

    test dev

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -25,7 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class VisitsServiceApplication {
-    
+    // hi
     public static void main(String[] args) {
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
